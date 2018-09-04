cca27fdc2d8a13e03cc43e35140cbdcd96b05706
romain <martinez.staps@gmail.com>
2018-09-04 15:41:12 -0400

    add figsize

--- python/fig_style.py
+++ python/fig_style.py
@@ -2,25 +2,47 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def set_style(style='mrtnz'):
+def get_style(style='mrtnz', figsize='one_half_column'):
+    style_dict = {}
+
+    golden_ratio = 1.618
+
+    if figsize == 'minimal':
+        width = 1.1811023622
+    elif figsize == 'one_col':
+        width = 3.5433070866
+    elif figsize == 'one_half_col':
+        width = 5.5118110236
+    elif figsize == 'two_col':
+        width = 7.4803149606
+    else:
+        raise NotImplementedError(f'Figsize {figsize} is not available')
+
+    style_dict.update({'figure.figsize': (width, width / golden_ratio)})
+
     if style == 'mrtnz' or style == 'mrtnz_tex':
-        style = {
-            'axes.labelsize': 20,
+        style_dict.update({
+            'axes.titlesize': 20,
             'axes.labelsize': 20,
             'xtick.labelsize': 16,
             'ytick.labelsize': 16,
             'legend.fontsize': 16
-        }
-        if style == 'mtrnz_tex':
-            style.update({'text.usetex': True})
-        plt.rcParams.update(style)
+        })
+        if style == 'mrtnz_tex':
+            style_dict.update({
+                'text.usetex': True,
+                'font.family': 'serif',
+            })
     else:
-        raise NotImplementedError(f'{style} is not available')
+        raise NotImplementedError(f'Style {style} is not available')
+    return style_dict
 
 
-def show(save=False):
-    sns.despine(offset=10, trim=True)
+def show(save=False, despine=True):
+    if despine:
+        sns.despine(offset=10, trim=True)
     plt.tight_layout()
     if save:
         plt.savefig(save, transparent=True)
     plt.show()
+
