abce75025c9277577d4547be4f73f3ea19a412a1
Romain Martinez <martinez.staps@gmail.com>
2024-06-10 15:46:27 -0400

    add pip and conda cert path

--- config.fish
+++ config.fish
@@ -4,6 +4,8 @@ end
 
 set -x PATH /Users/romainm/.local/bin $PATH
 set -x PATH /Users/romainm/.cargo/bin/ $PATH
+set -x PIP_CERT /usr/local/share/ca-certificates/manulife-cacert.perm
+set -x REQUESTS_CA_BUNDLE /usr/local/share/ca-certificates/manulife-cacert.perm
 
 # set alias for exa
 alias ls='lsd'
@@ -15,13 +17,12 @@ alias lt='ls --tree'
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" hook $argv | source
 else
     if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
         . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+        set -x PATH /opt/homebrew/Caskroom/miniconda/base/bin $PATH
     end
 end
 # <<< conda initialize <<<
-
