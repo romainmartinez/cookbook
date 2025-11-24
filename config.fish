778b89d1cf92b363445843853eb4c2095f2c1b7b
Romain Martinez <martinez.staps@gmail.com>
2025-11-24 15:43:50 -0500

    chore: clean up fish configuration comments

--- config.fish
+++ config.fish
@@ -56,7 +56,6 @@ end
 # remove the default greeting message
 set fish_greeting
 
-# edit the fish prompt to show only the current working directory
 function fish_prompt
     set_color $fish_color_cwd
     echo -n (prompt_pwd)
